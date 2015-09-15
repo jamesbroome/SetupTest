ba2f292706e1a7b059185aa579698eb3cc5668c3
james.broome <james.broome@endjin.com>
2015-09-15 09:35:09 +0100

    Initial commit

---
+++ Solutions/Endjin.BigBang.Specs/BigBang.feature
@@ -0,0 +1,10 @@
+﻿Feature: BigBang
+	In order to work smarter not harder
+	As an endjineer
+	I want to automate setting up a new project
+
+Scenario: Big Bang set up
+	Given I have created a new project
+	And I have created a new build definition
+	When I run the build
+	Then the everything should work first time
---
+++ Solutions/Endjin.BigBang.Specs/BigBangSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Endjin.BigBang.Specs
+{
+    [Binding]
+    public class BigBangSteps
+    {
+        [Given(@"I have created a new project")]
+        public void GivenIHaveCreatedANewProject()
+        {
+            
+        }
+        
+        [Given(@"I have created a new build definition")]
+        public void GivenIHaveCreatedANewBuildDefinition()
+        {
+
+        }
+        
+        [When(@"I run the build")]
+        public void WhenIRunTheBuild()
+        {
+
+        }
+        
+        [Then(@"the everything should work first time")]
+        public void ThenTheEverythingShouldWorkFirstTime()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
